19cd6fc7485244440b5c2f0fd7f24234fda4e095
DasStudioINC <dasstudiosinc@gmail.com>
2026-04-10 17:46:39 -0400

    first commit

---
+++ src/main/java/spidernetwork/com/core/Debug.java
@@ -0,0 +1,16 @@
+package spidernetwork.com.core;
+
+public class Debug {
+
+    public static void log(String msg){
+        System.out.println("[SpiderNetwork] " + msg);
+    }
+
+    public static void warn(String msg){
+        System.out.println("[SpiderNetwork WARNING] " + msg);
+    }
+    public static void error(String msg){
+        System.out.println("[SpiderNetwork] ERROR" + msg);
+    }
+
+}
