8e51b1b4b6e3f356dbf20afdd36ef6e4759dc1d0
DasStudioINC <dasstudiosinc@gmail.com>
2026-04-12 21:13:15 -0400

    Debug class update to not a static class

--- src/main/java/com/spidernetwork/core/Debug.java
+++ src/main/java/com/spidernetwork/core/Debug.java
@@ -2,14 +2,14 @@ package com.spidernetwork.core;
 
 public class Debug {
 
-    public static void log(String msg){
+    public void log(String msg){
         System.out.println("[SpiderNetwork] " + msg);
     }
 
-    public static void warn(String msg){
+    public void warn(String msg){
         System.out.println("[SpiderNetwork WARNING] " + msg);
     }
-    public static void error(String msg){
+    public void error(String msg){
         System.out.println("[SpiderNetwork] ERROR" + msg);
     }
 
