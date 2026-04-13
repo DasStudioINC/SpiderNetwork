1059c06ef25d793de27edb191a9389ed01c9977d
DasStudioINC <dasstudiosinc@gmail.com>
2026-04-12 21:28:05 -0400

    Debug function are back to static

--- src/main/java/com/spidernetwork/core/Debug.java
+++ src/main/java/com/spidernetwork/core/Debug.java
@@ -2,14 +2,13 @@ package com.spidernetwork.core;
 
 public class Debug {
 
-    public void log(String msg){
+    public static void log(String msg){
         System.out.println("[SpiderNetwork] " + msg);
     }
-
-    public void warn(String msg){
+    public static void warn(String msg){
         System.out.println("[SpiderNetwork WARNING] " + msg);
     }
-    public void error(String msg){
+    public static void error(String msg){
         System.out.println("[SpiderNetwork] ERROR" + msg);
     }
 
